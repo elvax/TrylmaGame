0302cc0d5ea24fc833b5ed603d26c6017630e5ed
Sebastian <elvaxumb@gmail.com>
2017-12-02 18:34:34 +0100

    Inital commit

---
+++ src/main/java/com/example/trylma/model/Game.java
@@ -0,0 +1,26 @@
+package com.example.trylma.model;
+
+import com.example.trylma.client.Player;
+
+public class Game {
+    /**
+     * A board has 61 fields
+     */
+    // TODO 1.ogarnac sposob na przechowanie lista czy 1- czy 2- wymiarowa tablica
+    // 2. czy kazde pole ma byc zainicjalizowanie Peg czy Peg tylko tam gdzie gracze maja pionki a reszta to null
+    private Peg[][] board;
+
+    /**
+     * The current player
+     */
+    private Player currentPlayer;
+
+    /**
+     * Returns whether the current state of the board is such that one of the players is a winner
+     */
+    public boolean hasWinner() {
+        //TODO ma zwrocic true jak jakis gracz zapelni przeciwlegly rog
+        return false;
+    }
+
+}
