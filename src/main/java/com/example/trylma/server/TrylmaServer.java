abff4e8bae0b92f9e2a27c0295432002377cba89
Sebastian <elvaxumb@gmail.com>
2017-12-02 18:03:02 +0100

    Add basic server

--- src/main/java/com/example/trylma/server/TrylmaServer.java
+++ src/main/java/com/example/trylma/server/TrylmaServer.java
@@ -1,4 +1,18 @@
 package com.example.trylma.server;
 
+import java.net.ServerSocket;
+
 public class TrylmaServer {
+    public static final int portNumber = 5555;
+
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket listener = new ServerSocket(portNumber);
+        System.out.println("Trylma server is running");
+        try {
+
+        } finally {
+            listener.close();
+        }
+    }
 }
