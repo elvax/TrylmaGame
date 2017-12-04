be0f28424911dfd92410dd09e94f34c4eedd0244
Sebastian <elvaxumb@gmail.com>
2017-12-04 12:27:01 +0100

    Rewrite whole TrylmaClient

--- src/main/java/com/example/trylma/client/TrylmaClient.java
+++ src/main/java/com/example/trylma/client/TrylmaClient.java
@@ -2,19 +2,46 @@ package com.example.trylma.client;
 
 import com.example.trylma.server.TrylmaServer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
- * Constructs client by connecting to server
+ *
  */
-//TODO tu ma byc GUI
 public class TrylmaClient {
     private Socket socket;
+    BufferedReader input;
+    PrintWriter output;
 
-    public TrylmaClient(String serverAdress) throws Exception{
 
-        // Setup networking
-        socket = new Socket(serverAdress, TrylmaServer.portNumber);
+
+    public TrylmaClient() {
+        //TODO odpalic GUI
+    }
+
+    private void run() throws IOException {
+        //TODO zrobic jakies gettery na paramtery do tworzenia socketu
+
+        // Make connection and initialize streams
+        Socket socket = new Socket("localhost", TrylmaServer.portNumber);
+        output = new PrintWriter(socket.getOutputStream(), true);
+        input = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        // Process all messages from server
+        while (true) {
+            String line = input.readLine();
+            System.out.println(line);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        TrylmaClient client = new TrylmaClient();
+        client.run();
     }
 
 }
