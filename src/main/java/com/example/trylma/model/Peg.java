7aa68f071236459b28903c43c8dd06dcfdef075c
paralelna <skonsternowana@gmail.com>
2017-12-06 11:48:43 +0100

    Add fields to class Peg.

--- src/main/java/com/example/trylma/model/Peg.java
+++ src/main/java/com/example/trylma/model/Peg.java
@@ -1,7 +1,15 @@
 package com.example.trylma.model;
 
+import java.awt.*;
+
 /*
  * Every peg has his owner and imformations needed to draw itself
  */
 public class Peg {
+    int i;
+    int j;
+    int xDraw;
+    int yDraw;
+    Image image;
+    String owner;
 }
