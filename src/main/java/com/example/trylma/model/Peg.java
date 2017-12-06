0ef395e0a8a66f58653a37f0dccfc7cf8e62b9cf
Sebastian <elvaxumb@gmail.com>
2017-12-06 21:04:11 +0100

    Add constructor

--- src/main/java/com/example/trylma/model/Peg.java
+++ src/main/java/com/example/trylma/model/Peg.java
@@ -13,4 +13,9 @@ public class Peg {
     Image image;
     int ownerID;
 
+    public Peg(int i, int j, int ownerID) {
+        this.i = i;
+        this.j = j;
+        this.ownerID = ownerID;
+    }
 }
