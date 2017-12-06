3cc5e591523d76db867b936fb2ec8e42f9b0136a
Sebastian <elvaxumb@gmail.com>
2017-12-06 21:36:38 +0100

    Add method to initalize board

--- src/main/java/com/example/trylma/model/Game.java
+++ src/main/java/com/example/trylma/model/Game.java
@@ -21,4 +21,94 @@ public class Game {
         return false;
     }
 
+    public void initalizeBoard() {
+        board = new Peg[17][13];
+
+        int i = 0;
+        for(int j=0; j<6; j++) { board[i][j]=new Peg(i,j,-1); }
+        board[i][6] = new Peg(i, 6, 0);
+        for(int j=0; j<6; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<6; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=6; j<8; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=8; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<5; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=5; j<8; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=8; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<5; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=5; j<9; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=9; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+       // for(int j=0; j<5; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=0; j<13; j++) { board[i][j]=new Peg(i,j,0); }
+       // for(int j=9; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+       // for(int j=0; j<5; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=5; j<12; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=12; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<1; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=1; j<12; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=12; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<1; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=1; j<11; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=11; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<2; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=2; j<11; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=11; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<2; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=2; j<12; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=12; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<1; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=1; j<12; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=12; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<1; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=1; j<13; j++) { board[i][j]=new Peg(i,j,0); }
+       // for(int j=12; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+      //  for(int j=0; j<1; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=0; j<13; j++) { board[i][j]=new Peg(i,j,0); }
+        // for(int j=12; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<5; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=5; j<9; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=9; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<5; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=5; j<8; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=8; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<6; j++) { board[i][j]=new Peg(i,j,-1); }
+        for(int j=6; j<8; j++) { board[i][j]=new Peg(i,j,0); }
+        for(int j=8; j<13; j++) { board[i][j]=new Peg(i,j,-1); }
+
+        ++i;
+        for(int j=0; j<6; j++) { board[i][j]=new Peg(i,j,-1); }
+        board[i][6] = new Peg(i, 6, 0);
+        for(int j=0; j<6; j++) { board[i][j]=new Peg(i,j,-1); }
+    }
+
+
 }
