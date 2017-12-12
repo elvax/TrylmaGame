57086fd1ba17b2ca44fc66322ca3ac8a4c0da99b
paralelna <skonsternowana@gmail.com>
2017-12-12 14:59:16 +0100

    Write a testIsClicked.

---
+++ src/test/java/com/example/trylma/com/example/trylma/model/PegTest.java
@@ -0,0 +1,26 @@
+package com.example.trylma.com.example.trylma.model;
+
+import com.example.trylma.model.Peg;
+import junit.framework.TestCase;
+
+public class PegTest extends TestCase {
+    Peg p = null;
+    int i = 8;
+    int j = 8;
+    int xDraw = 30;
+    int yDraw = 35;
+    int ownerID = 1;
+
+    protected void setUp() throws Exception {
+        p = new Peg(i,j,ownerID);
+        p.setXY(xDraw, yDraw);
+        assertNotNull(p);
+    }
+
+    public void testIsClicked() {
+        boolean result = p.isClicked(55,50);
+        boolean result2 = p.isClicked(60,35);
+        assertTrue(result);
+        assertFalse(result2);
+    }
+}
