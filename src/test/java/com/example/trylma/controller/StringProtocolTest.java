19c758db122ec7a17b1b8d709ba2035869b9e3a8
Sebastian <elvaxumb@gmail.com>
2017-12-17 22:31:32 +0100

    Add class to test TrylmaStringProtocol

---
+++ src/test/java/com/example/trylma/controller/StringProtocolTest.java
@@ -0,0 +1,30 @@
+package com.example.trylma.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+public class StringProtocolTest {
+    TrylmaStringProtocol protocol;
+    @Before
+    public void initialize() throws Exception {
+        protocol = new TrylmaStringProtocol();
+    }
+
+    @Test
+    public void getXmousePressedTest() {
+        String message = "PRESSED (123,456)";
+        int result = protocol.getXmousePressed(message);
+        assertEquals(123,result);
+    }
+
+    @Test
+    public void getYmousePressedTest() {
+        String message = "PRESSED (123,456)";
+        int result = protocol.getYmousePressed(message);
+        assertEquals(456,result);
+    }
+
+}
