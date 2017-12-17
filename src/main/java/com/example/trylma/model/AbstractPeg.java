594a57aafbcb8c2730b9c63f0f65b39645ec9ef7
Sebastian <elvaxumb@gmail.com>
2017-12-17 22:31:09 +0100

    Add Abstract class AbstractPeg

---
+++ src/main/java/com/example/trylma/model/AbstractPeg.java
@@ -0,0 +1,15 @@
+package com.example.trylma.model;
+
+import java.awt.*;
+import java.io.IOException;
+
+public abstract class AbstractPeg {
+    abstract public void doDraw(Graphics g);
+    abstract public void setImage(int ownerId) throws IOException;
+    abstract public int getOwnerID();
+    abstract public void setXY(int x, int y);
+    abstract public void changeOwnerID(int newOwnerID);
+    abstract public boolean isClicked(int x, int y);
+//    abstract public AbstractPeg getPeg(int i, int j) throws IllegalArgumentException;
+
+}
