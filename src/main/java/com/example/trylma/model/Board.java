7c64b9d257351af59bd89784f061301580f47fdd
paralelna <skonsternowana@gmail.com>
2017-12-28 03:40:26 +0100

    Add methods to class Board, the most important: move which accepts the client's move, and the method which returns the range of possible moves.

--- src/main/java/com/example/trylma/model/Board.java
+++ src/main/java/com/example/trylma/model/Board.java
@@ -3,6 +3,8 @@ package com.example.trylma.model;
 import java.awt.*;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Board implements Serializable{
     public static int I_BOARD_SIZE = 17;
@@ -228,17 +230,136 @@ public class Board implements Serializable{
     }
 
     //green
-    public void fillSectorSix(){
-        for(int j=9; j<13; j++) {
-            board[12][j]=new Peg(12,j,6);
+    public void fillSectorSix() {
+        for (int j = 9; j < 13; j++) {
+            board[12][j] = new Peg(12, j, 6);
         }
-        for(int j=10; j<13; j++){
-            board[11][j]=new Peg(11,j,6);
+        for (int j = 10; j < 13; j++) {
+            board[11][j] = new Peg(11, j, 6);
         }
-        for(int j=10; j<12; j++){
-            board[10][j]=new Peg(10,j,6);
+        for (int j = 10; j < 12; j++) {
+            board[10][j] = new Peg(10, j, 6);
         }
-        board[9][11]=new Peg(9,2,6);
+        board[9][11] = new Peg(9, 2, 6);
+    }
+
+    public AbstractPeg getPeg(int i, int j){
+        return board[i][j];
+    }
+
+    //Metoda szukająca wszystkich sąsiadów tych zajętych i pustych
+    public List<AbstractPeg> findNeighbours(AbstractPeg peg){
+        List<AbstractPeg> neighbours = new ArrayList<AbstractPeg>();
+        int i = peg.geti();
+        int j = peg.getj();
+        int bi = i - 1;
+        int ei = i + 1;
+        int bj = j - 1;
+        int ej = j + 1;
+        if (i == 0) { bi = i; }
+        else if (i == I_BOARD_SIZE - 1) { ei = i; }
+        if (j == 0) { bj = j; }
+        else if (j == J_BOARD_SIZE - 1) { bj = j; }
+        for (int k = bi; k <= ei; k++) {
+            for (int t = bj; t <= ej; t++) {
+                if (((t == j - 1 && i % 2 == 0) && (k == i - 1 || k == i + 1)) || k == i && t == j) {}
+                else if ((t == j + 1 && i % 2 != 0) && (k == i + 1 || k == i - 1)) {}
+                else { neighbours.add(board[k][t]);
+                    //System.out.println("Sasiad :" + k + " " + t);
+                }
+            }
+        }
+        return neighbours;
+    }
+    //zwraca pustych sąsiadów czyli tych które mają ID 0 i może się tam przemieścić od razu
+    public List<AbstractPeg> findEmptyNeighbours(AbstractPeg peg){
+        List<AbstractPeg> neighbours = findNeighbours(peg);
+        List<AbstractPeg> emptyNeighbours = new ArrayList<AbstractPeg>();
+        for(int i = 0; i < neighbours.size(); i++){
+            AbstractPeg p = neighbours.get(i);
+            if(p.getSectorID() == 0){
+                emptyNeighbours.add(p);
+                //System.out.println("Sąsiad pusty: " + neighbours.get(i).geti() + " " + neighbours.get(i).getj());
+            }
+        }
+        return emptyNeighbours;
+    }
+
+    //zwraca sąsiadów którzy są zajęci tzn mają id rózny od 0 i -1, należą do jakiegoś gracza.
+    public List<AbstractPeg> findNotEmptyNeighbours(AbstractPeg peg){
+        List<AbstractPeg> neighbours = findNeighbours(peg);
+        List<AbstractPeg> notEmptyNeighbours = new ArrayList<AbstractPeg>();
+        for(int i = 0; i < neighbours.size(); i++){
+            AbstractPeg p = neighbours.get(i);
+            if(p.getSectorID() != 0 && p.getSectorID() != -1){
+                notEmptyNeighbours.add(p);
+                //System.out.println("Sąsiad pełny: " + p.geti() + " " + p.getj());
+            }
+        }
+        return notEmptyNeighbours;
+    }
+
+    //sprawdza czy ruch był poprawny, tzn czy jest w liscie mozliwych ruchów i jeśli jest zmienia identyfikatory
+    // czyli nanosi ruch na planszę.
+    public void move(AbstractPeg p, int i, int j){
+        List<AbstractPeg> neighbours = correctMoves(p);
+        for(int t=0; t<neighbours.size(); t++){
+            AbstractPeg temp = neighbours.get(t);
+            //System.out.println("Sasiad: " + temp.geti() + " " + temp.getj());
+            if(temp.isClicked(i,j)==true && temp.getSectorID()==0){
+                board[temp.geti()][temp.getj()]=new Peg(temp.geti(),temp.getj(),p.getSectorID());
+                board[p.geti()][p.getj()]=new Peg(p.geti(),p.getj(),0);
+            }
+        }
+    }
+    //zwraca listę wszystkich możliwych ruchów tzn, sasiadów którzy mają id=0 uzupełnionych o skoki.
+    public List<AbstractPeg> correctMoves(AbstractPeg p){
+        List<AbstractPeg> moves = findEmptyNeighbours(p);
+        List<AbstractPeg> notEmpty = findNotEmptyNeighbours(p);
+        for(int i=0; i < notEmpty.size(); i++){
+            AbstractPeg neighbour = notEmpty.get(i);
+            int j_move = -1;
+            int i_move = -1;
+            int i_neighbour = neighbour.geti();
+            int j_neighbour = neighbour.getj();
+            if (p.geti() < i_neighbour && p.geti() < I_BOARD_SIZE - 2) {
+                i_move = p.geti() + 2;
+            } else if (p.geti() > i_neighbour && p.geti() > 1){
+                i_move = p.geti() - 2;
+            }
+            if(p.getj() < j_neighbour && p.getj() < J_BOARD_SIZE - 1){
+                j_move = p.getj() + 1;
+            } else if(p.getj() > j_neighbour && p.getj() > 0) {
+                j_move = p.getj() - 1;
+            }
+            if(p.geti()%2!=0 && p.getj() == j_neighbour){
+                j_move = p.getj() + 1;
+            } else if(p.geti()%2==0 && p.getj() == j_neighbour){
+                j_move = p.getj() - 1;
+            }
+            if(p.geti() == i_neighbour){
+                i_move=p.geti();
+                if(p.getj() <= j_neighbour && p.getj() < J_BOARD_SIZE - 2){
+                    j_move = p.getj() + 2;
+                } else if(p.getj() > j_neighbour && p.getj() > 1) {
+                    j_move = p.getj() - 2;
+                }
+            }
+            if(i_move!=-1 && j_move!=-1 && board[i_move][j_move].getSectorID()==0){
+                moves.add(board[i_move][j_move]);
+            }
+        }
+        return moves;
+    }
+
+
+    public void printBoard(){
+        for(int o=0; o<17; o++){
+            for(int k=0; k<13; k++){
+                System.out.print(board[o][k].getSectorID());
+            }
+            System.out.println("");
+        }
     }
 
 }
