c6b13ad32b22b9c790f6fd8e09c0662d5c4105fa
Sebastian <elvaxumb@gmail.com>
2017-12-30 11:28:26 +0100

    Remove unnecessary methods and fields in class TrylmaServer

--- src/main/java/com/example/trylma/server/TrylmaServer.java
+++ src/main/java/com/example/trylma/server/TrylmaServer.java
@@ -19,14 +19,7 @@ public class TrylmaServer {
     public static final int portNumber = 5555;
     TrylmaStringProtocol protocol;
     Game currentGame;
-    int currentPlayer=0;
     int numberOfPlayers;
-    AbstractPeg activePeg;
-
-    BufferedReader inputLineFromStdIn = new BufferedReader(new InputStreamReader(System.in));
-    String fromUser;
-
-
 
     // List of clients connected to server
     private List<PlayerThread> clietnsThreadsList = new ArrayList<PlayerThread>();
@@ -42,8 +35,6 @@ public class TrylmaServer {
     public TrylmaServer() {
         protocol = new TrylmaStringProtocol();
         currentGame = new Game();
-
-
         try {
             serverSocket = new ServerSocket(portNumber);
         } catch (IOException e) {
@@ -58,27 +49,26 @@ public class TrylmaServer {
         currentGame.setBoardForPlayers(numberOfPlayers);
         currentGame.setOrderOfMoves();
 
-
+        // Wait for clients to connect
         for (Integer id : currentGame.getActiveSectorsID()) {
             clietnsThreadsList.add(new PlayerThread(serverSocket.accept(), id));
         }
 
         System.out.println(numberOfPlayers + " clients connected");
 
+        // Start client's threads
         for (PlayerThread pt : clietnsThreadsList) {
             pt.start();
         }
-
 
     }
 
     public static void main(String[] args) throws Exception {
         TrylmaServer server = new TrylmaServer();
-        server.waitForClients(1);
+        server.waitForClients(3);
     }
 
     private class PlayerThread extends Thread {
-
         int id;
         Socket socket;
         BufferedReader input;
@@ -91,56 +81,57 @@ public class TrylmaServer {
             this.id = id;
         }
 
+        private void initializeStreams() throws IOException{
+            //Initalize streams
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+            // add stream to sets
+            writers.add(output);
+            readers.add(input);
+            objectOutput.add(objectOutputStream);
+        }
+
         public void run() {
             try {
-                List<AbstractPeg> pegsToChange = new ArrayList<AbstractPeg>();
-
-                //Initalize streams
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                output = new PrintWriter(socket.getOutputStream(), true);
-                objectInputStream = new ObjectInputStream(socket.getInputStream());
-                objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-
-                // add stream to sets
-                writers.add(output);
-                readers.add(input);
-                objectOutput.add(objectOutputStream);
+                initializeStreams();
 
                 //send initial board state to connected client
                 objectOutputStream.writeObject(currentGame.getBoardOfTrylma());
 
+                List<AbstractPeg> pegsToChange = new ArrayList<AbstractPeg>();
                 while (true) {
-
                     if (currentGame.getCurrentID() == this.id) {
                         String fromClient = input.readLine();
-                        //System.out.println("from client " + id + " " + fromClient);
 
                         if (fromClient.startsWith("PRESSED")) {
                             int x = protocol.getXmousePressed(fromClient);
                             int y = protocol.getYmousePressed(fromClient);
                             AbstractPeg pegClicked = currentGame.findActive(x, y, id);
                             System.out.println(pegClicked);
-                            if(pegClicked!=null){
-                                System.out.println("PRESSED:" + pegClicked);
-                                List<AbstractPeg> possibilities = currentGame.setPossibleMoves(pegClicked);
-                                if(possibilities.size()>0){
-                                    AbstractPeg[] array2 = new AbstractPeg[possibilities.size()];
-                                    for(int i=0; i<possibilities.size(); i++){
-                                        AbstractPeg p = possibilities.get(i);
-                                        //pegsToChange.add(possibilities.get(i));
-                                        pegsToChange.add(p);
-                                        //array2[i] = p;
-                                        //System.out.println("ECH:" + p);
-                                        array2[i]=new Peg(p.geti(),p.getj(),p.getSectorID());
-                                        //System.out.println("ToSEND1: i="+ array2[i].geti() + " j= " + array2[i].getj() + " id=" + array2[i].getSectorID());
 
-                                    }
-                                    for (ObjectOutputStream objectOut : objectOutput) {
-                                        objectOut.writeObject(array2);
-                                    }
+                            System.out.println("PRESSED:" + pegClicked);
+                            List<AbstractPeg> possibilities = currentGame.setPossibleMoves(pegClicked);
+                            if(possibilities.size()>0){
+                                AbstractPeg[] array2 = new AbstractPeg[possibilities.size()];
+                                for(int i=0; i<possibilities.size(); i++){
+                                    AbstractPeg p = possibilities.get(i);
+                                    //pegsToChange.add(possibilities.get(i));
+                                    pegsToChange.add(p);
+                                    //array2[i] = p;
+                                    //System.out.println("ECH:" + p);
+                                    array2[i]=new Peg(p.geti(),p.getj(),p.getSectorID());
+                                    //System.out.println("ToSEND1: i="+ array2[i].geti() + " j= " + array2[i].getj() + " id=" + array2[i].getSectorID());
+
                                 }
-
+                                for (ObjectOutputStream objectOut : objectOutput) {
+                                    objectOut.writeObject(array2);
+                                }
                             }
+
+
                             fromClient = input.readLine();
                             if (fromClient.startsWith("RELEASED") && pegClicked!=null) {
                                 //System.out.println("Clicked2: i="+ pegClicked.geti() + " j= " + pegClicked.getj());
