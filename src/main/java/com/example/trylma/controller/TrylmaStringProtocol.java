1738feb4c5be835d80ed354fb90dc765cc086f68
Sebastian <elvaxumb@gmail.com>
2018-01-02 10:32:53 +0100

    Remove unnecessary methods

--- src/main/java/com/example/trylma/controller/TrylmaStringProtocol.java
+++ src/main/java/com/example/trylma/controller/TrylmaStringProtocol.java
@@ -8,11 +8,10 @@ import java.util.regex.Pattern;
 
 // TODO klasa ma być singletonem
 public class TrylmaStringProtocol {
+
     private static TrylmaStringProtocol instance;
 
-    private static int xPressed, yPressed;
     private String regex = "(\\d+),(\\d+)";
-    String regex2 = "(\\d+),(\\d+)\\d";
     private Pattern pattern;
     private Matcher m;
 
@@ -39,38 +38,11 @@ public class TrylmaStringProtocol {
     }
 
     public static String sendMousePressed(int x, int y) {
-        xPressed = x;
-        yPressed = y;
         return "PRESSED (" + x + "," + y + ")";
     }
 
     public static String sendEndTurn() {
         return "ENDTURN";
-    }
-
-    public static String sendChange(int i, int j, int id) {
-        return "CHANGE (" + i + "," + j + ")" + id;
-    }
-
-    public int DecodeMoveX(String message) {
-        pattern = Pattern.compile(regex2);
-        m = pattern.matcher(message);
-        m.find();
-        return Integer.parseInt(m.group(1));
-    }
-
-    public int DecodeMoveY(String message) {
-        pattern = Pattern.compile(regex2);
-        m = pattern.matcher(message);
-        m.find();
-        return Integer.parseInt(m.group(2));
-    }
-
-    public int DecodeMoveID(String message) {
-        pattern = Pattern.compile(regex2);
-        m = pattern.matcher(message);
-        m.find();
-        return Integer.parseInt(m.group(3));
     }
 
     public int getXmousePressed(String message) {
